76745739ad931c8f7bf4f6aac6cc1f19bcff7146
Wxve <wxvedev@gmail.com>
2024-12-07 20:18:36 +0000

    init: @astrojs/vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
   vite: {
     resolve: {
       alias: {
@@ -13,4 +16,6 @@ export default defineConfig({
       },
     },
   },
+
+  adapter: vercel(),
 });
